4365507a75736f7af2d9699bc6f56116eca2df45
Henrique do Prado Linhares <linhares.h@gmail.com>
2015-04-24 16:38:03 -0300

    Outras alterações feitas. Tentando acertar as organizações do search for event e scheduler.

--- SchedulerSimulator/src/schedulersimulator/SearchForEvent.java
+++ SchedulerSimulator/src/schedulersimulator/SearchForEvent.java
@@ -5,29 +5,40 @@
  */
 package schedulersimulator;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Henrique
  */
 public class SearchForEvent {
-private int time;
+
+    private static int time;
 
     public SearchForEvent() {
         this.time = 0;
     }
 
-public static void searchIteration(int time, Scheduler scheduler, Tasks tasks){
-    System.out.println(tasks.searchForArrivalsAtTime(time).toString());
-}
-    
-    
-public static void EventSearcher(Scheduler scheduler, Tasks tasks){
-    System.out.println("Im on event searcher now.");
-    searchIteration(0, scheduler, tasks);
-    
-}
+    public static void searchIteration(int time, Scheduler scheduler, Tasks tasks) {
+        ArrayList<Task> T = tasks.searchForArrivalsAtTime(time);
+    }
 
-    
-    
-    
+    public static boolean allTasksHaveFinished(Tasks tasks) {
+        return (tasks.getTaskList().isEmpty());
+    }
+
+    public static void EventSearcher(Scheduler scheduler, Tasks tasks) {
+        SearchForEvent.time = 0;
+
+        while (!allTasksHaveFinished(tasks)) {
+
+            System.out.println("Search for event is iterating");
+
+            searchIteration(SearchForEvent.time, scheduler, tasks);
+
+            SearchForEvent.time++;
+        }
+
+    }
+
 }
